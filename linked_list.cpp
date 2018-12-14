b91daf722d02eaf371c11f22ab630ca9740a51d2
TrueCookie <artemogloblin56@gmail.com>
2018-12-14 13:51:36 +0300

    grouped files

---
+++ linked_list.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+//#include "data.h"
+#include "linked_list.h"
+
+void linked_list::push_back(int input) {
+	data* nData = new data;
+	nData->value = input;
+	if (this->is_empty()) {
+		head = nData;
+		tail = new data;
+	}
+	else {
+		tail->prev->next = nData;
+	}
+	tail->prev = nData;
+	nData->next = tail;
+}
+
+void linked_list::push_front(int input) {
+	data* nData = new data;
+	nData->value = input;
+	if (this->is_empty()) {
+		tail = new data;
+		tail->prev = nData;
+		nData->next = tail;
+	}
+	else {
+		head->prev = nData;
+		nData->next = head;
+	}
+	head = nData;
+}
+
+void linked_list::read() {
+	data* tmp = head;
+	while (tmp != tail) {
+		std::cout << tmp->value << "; ";
+		tmp = tmp->next;
+	}std::cout << std::endl;
+}
+
+void linked_list::reverse() {
+	data* move = head;
+	data* tmp = nullptr;
+	while (move != tail) {
+		tmp = move->next;
+		move->next = move->prev;
+		move->prev = tmp;
+
+		move = move->prev;
+	}
+	data* tmp_end = head;
+	head = tail->prev;
+	head->prev = nullptr;
+	tail->next = nullptr;
+	tmp_end->next = tail;
+}
+
+bool linked_list::is_empty() {
+	return head == tail;
+}
+
+size_t linked_list::size() {
+	size_t count = 0;
+	data* tmp = head;
+	while (tmp != tail) {
+		count++;
+		tmp = tmp->next;
+	}
+	return count;
+}
+
+int linked_list::remove(int val) {
+	data* p_val = find(val);
+	if (p_val != tail) {
+		p_val->prev->next = p_val->next;
+		p_val->next->prev = p_val->prev;
+	}
+	else {
+		std::cout << "Value not found" << std::endl;
+	}return p_val->value;
+}
+
+data* linked_list::find(int val) {
+	data* tmp = head;
+	while (tmp != tail) {
+		if (tmp->value == val) {
+			break;
+		}
+		else {
+			tmp = tmp->next;
+		}
+	}return tmp;
+}
